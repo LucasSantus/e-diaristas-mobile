c3e3e3769bfbf74d87a6d8dce6a398033b462204
Lucas Santus <leos9877@gmail.com>
2021-06-18 10:59:24 -0300

    adicionando pages

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,23 @@
 module.exports = function(api) {
   api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ["module-resolver", {
-        alias: {
-          data: './src/data',
-          ui: './src/ui',
-          pages: './src/pages',
-          '@assets': './assets',
-          '@styles': './src/ui/styles'
-        }
-      }]
-    ]
-  };
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    alias: {
+                        data: './src/data',
+                        ui: './src/ui',
+                        pages: './src/pages',
+                        '@assets': './assets',
+                        '@styles': './src/ui/styles',
+                    },
+                },
+            ],
+            ['react-native-paper/babel'],
+        ],
+    };
 };
+
+
---
+++ src/ui/router/router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationTheme } from 'ui/themes/app-theme';
+
+const Router: React.FC = () => {
+    return (
+        <NavigationContainer theme={NavigationTheme}>
+
+        </NavigationContainer>
+    )
+}
+
+export default Router;
